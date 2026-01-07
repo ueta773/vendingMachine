e8676457f86b00971f5996029281edb9540149f6
ueta773 <ueta.sa@mira-x.co.jp>
2026-01-08 01:09:09 +0900

    Refactor: replace macros with enum/const and improve menu input

--- main.c
+++ main.c
@@ -1,57 +1,85 @@
 #include <stdio.h>
 
-#define COIN_10 10
-#define COIN_50 50
-#define COIN_100 100
-#define COIN_500 500
-#define INITIAL_VALUE 0
-#define PRICE_OCHA 100
-#define PRICE_COFFEE 120
-#define PRICE_JUICE 140
-#define NENU_BUY 1
-#define NENU_MAINTENANCE 2
-#define NENU_EXIT 99
-#define MIN_SYOUHIN_NUMBER 1
-#define INFINITE_LOOP 1
+/* メニュー番号(将来的にヘッダーファイルへ移行) */
+typedef enum{
+    MENU_NONE = 0,
+    MENU_BUY = 1,
+    MENU_MAINTENANCE = 2,
+    MENU_EXIT = 99
+} Menu;
+
+/* (将来的にヘッダーファイルへ移行) */
+extern const int COIN_10;
+extern const int COIN_50;
+extern const int COIN_100;
+extern const int COIN_500;
+
+/* 商品価格(将来的にヘッダーファイルへ移行) */
+extern const int DRINK01_PRICE;
+extern const int DRINK02_PRICE;
+extern const int DRINK03_PRICE;
+
+/* extern constの定義(実体) */
+const int COIN_10 = 10;
+const int COIN_50 = 50;
+const int COIN_100 = 100;
+const int COIN_500 = 500;
+
+const int DRINK01_PRICE = 100;
+const int DRINK02_PRICE = 120;
+const int DRINK03_PRICE = 140;
+
+static const int MIN_SYOUHIN_NUMBER = 1;
+
+/* 商品データ */
+static const char * const syouhinName[] = {"お茶","コーヒー","ジュース"};
+static const int prices[] = {DRINK01_PRICE,DRINK02_PRICE,DRINK03_PRICE};
+static const int itemCount = (int)(sizeof(prices) / sizeof(prices[0]));
 
 void inputCoins(int *jyuu,int *gojyuu, int *hyaku,int *gohyaku);
 
 int main()
 {
-    int menuNumber = INITIAL_VALUE;
-    int jyuuYen = INITIAL_VALUE;
-    int gojyuuYen = INITIAL_VALUE;
-    int hyakuYen = INITIAL_VALUE;
-    int gohyakuYen = INITIAL_VALUE;
-    int tounyuuKingaku = INITIAL_VALUE;
-    int syouhinNumber = INITIAL_VALUE;
-    const char *syouhinName[] = {"お茶","コーヒー","ジュース"};
-    int prices[] = {PRICE_OCHA,PRICE_COFFEE,PRICE_JUICE};
-    int itemCount = 3;
-    int otsuri = INITIAL_VALUE;
+    Menu menuNumber = MENU_NONE;
+    int jyuuYen = 0;
+    int gojyuuYen = 0;
+    int hyakuYen = 0;
+    int gohyakuYen = 0;
+    int tounyuuKingaku = 0;
+    int syouhinNumber = 0;
+    int otsuri = 0;
 
 
-    while (menuNumber != NENU_BUY && menuNumber != NENU_MAINTENANCE && menuNumber != NENU_EXIT)
+    while (1)
     {
         printf("\n自動販売機プログラム\n");
-        // printf("【メニュー選択】実行するメニュー番号を入力してください。\n");
-        printf("【メニュー選択】%d：商品購入、%d：メンテナンス、%d：終了\n",NENU_BUY,NENU_MAINTENANCE,NENU_EXIT);
+        printf("【メニュー選択】%d：商品購入、%d：メンテナンス、%d：終了\n",MENU_BUY,MENU_MAINTENANCE,MENU_EXIT);
 
-        if (scanf("%d",&menuNumber) != 1)
+        int inputMenu = 0;
+        if (scanf("%d",&inputMenu) != 1)
         {
             printf("実行するメニュー番号を入力してください。\n");
             while (getchar() != '\n');
             continue;
         }
+
+        if (inputMenu ==  MENU_BUY || inputMenu == MENU_MAINTENANCE || inputMenu == MENU_EXIT){
+            menuNumber = (Menu)inputMenu;
+        } else{
+            printf("\n正しい番号を入力してください。\n");
+            menuNumber = MENU_NONE;
+            continue;
+        }
+
         switch (menuNumber)
         {
-            case NENU_BUY:
+            case MENU_BUY:
             {
-                int syouhinPrice = INITIAL_VALUE;
+                int syouhinPrice = 0;
 
                 // 商品一覧
                 printf("\n【取扱商品】\n");
-                for (int i = INITIAL_VALUE; i < itemCount;i ++)
+                for (int i = 0; i < itemCount;i ++)
                 {
                     printf("%d：%s(%d円)\n",i + MIN_SYOUHIN_NUMBER,syouhinName[i],prices[i]);
                 }
@@ -63,14 +91,14 @@ int main()
                 tounyuuKingaku = (jyuuYen*COIN_10) + (gojyuuYen*COIN_50) + (hyakuYen*COIN_100) + (gohyakuYen*COIN_500);
 
                 // 合計額が0の場合、メニューへ戻る
-                if (tounyuuKingaku == INITIAL_VALUE)
+                if (tounyuuKingaku == 0)
                 {
                     printf("\n投入金額が0のためメニューに戻ります。\n");
-                    menuNumber = INITIAL_VALUE;
+                    menuNumber = MENU_NONE;
                     break;
                 }
 
-                while (INFINITE_LOOP)
+                while (1)
                 {
                     printf("\n購入する商品の番号を入力してください。\n");
                     scanf("%d",&syouhinNumber);
@@ -89,11 +117,11 @@ int main()
                 // 投入金額と商品価格の比較
                 if (tounyuuKingaku < syouhinPrice)
                 {
-                    int fusokuKingaku = INITIAL_VALUE;
+                    int fusokuKingaku = 0;
                     fusokuKingaku = syouhinPrice - tounyuuKingaku;
 
                     // 投入金額が不足していた場合の処理
-                    while (fusokuKingaku > INITIAL_VALUE)
+                    while (fusokuKingaku > 0)
                     {
                         printf("\n投入金額が%d円不足しています。\n商品価格：%d円、投入金額：%d円\n",fusokuKingaku,syouhinPrice,tounyuuKingaku);
 
@@ -114,15 +142,17 @@ int main()
                 otsuri = tounyuuKingaku - syouhinPrice;
                 printf("おつり%d円\n",otsuri);
 
-                menuNumber = INITIAL_VALUE;
+                menuNumber = MENU_NONE;
                 break;
             }
-            case NENU_MAINTENANCE:
+            case MENU_MAINTENANCE:
                 printf("%d:メンテナンス\n",menuNumber);
+                return 0;
                 break;
 
-            case NENU_EXIT:
+            case MENU_EXIT:
                 printf("%d:終了\n",menuNumber);
+                return 0;
                 break;
 
             default:
